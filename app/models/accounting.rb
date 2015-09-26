e3ed7d1a4bf3d669dc07a6d42cf79fee53bb1aae
Alejandro Babio <alejandro.e.babio@hotmail.com>
2015-09-26 14:31:09 -0300

    Validate presence of book and commodity at Accounting, remove actions at AccountingsController

--- app/models/accounting.rb
+++ app/models/accounting.rb
@@ -2,6 +2,9 @@ class Accounting < ActiveRecord::Base
   belongs_to :commodity
   belongs_to :book
 
+  validates :commodity, presence: true
+  validates :book, presence: true
+
   def to_s
     "#{book} - #{commodity}"
   end
--- app/views/accountings/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @accounting, :id, :commodity_id, :book_id, :created_at, :updated_at
