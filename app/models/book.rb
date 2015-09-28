1e2b9beb7885a6bd39307a8fe595cff29c37c048
Alejandro Babio <alejandro.e.babio@hotmail.com>
2015-09-27 23:15:11 -0300

    option 2

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,15 @@ class Book < ActiveRecord::Base
   has_many :commodities, through: :accountings
   has_many :companies, through: :accountings
 
+  before_save :set_companies
+
   def to_s
     name
+  end
+
+  private
+
+  def set_companies
+    self.company_ids = Company.all.map(&:id)
   end
 end
